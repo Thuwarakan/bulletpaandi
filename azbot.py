33c80f22e5dd3ef4fdc43e90bdaf153df35103f4
Thuwarakan <mothuwarakan@gmail.com>
2023-02-03 00:52:40 +0530

    Create azbot.py

---
+++ azbot.py
@@ -0,0 +1,50 @@
+import subprocess
+import threading
+
+def run_command(command):
+    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    return result.stdout.decode("utf-8").strip()
+
+def create_vm(region):
+    # Set the default location to the current region
+    run_command(f"az configure --defaults location={region}")
+
+    # Create a resource group for the virtual machine
+    resource_group_name = f"myrg-{region}"
+    run_command(f"az group create --name {resource_group_name} --location {region}")
+
+    # Create a virtual machine with size `f8s`
+    vm_name = f"myvm-{region}"
+    run_command(f"az vm create --resource-group {resource_group_name} --name {vm_name} --image UbuntuLTS --admin-username azureuser --generate-ssh-keys --size f8s")
+
+    print(f"Virtual machine '{vm_name}' created in region '{region}'")
+
+    # Run the command "apt update" on the virtual machine
+    run_command(f"az vm run-command invoke --resource-group {resource_group_name} --name {vm_name} --command-id RunShellScript --scripts 'apt update'")
+
+    print(f"Package list updated on virtual machine '{vm_name}' in region '{region}'")
+
+# Get the list of all Azure regions
+output = run_command("az account list-locations --query '[].name' --output tsv")
+regions = output.split("\n")
+
+# Get the list of regions eligible for virtual machine size `f8s`
+f8s_regions = []
+for region in regions:
+    # Check if the region is eligible for virtual machine size `f8s`
+    output = run_command(f"az vm list-skus --location {region} --query '[?contains(name, \"f8s\")].locationInfo[].location' --output tsv")
+    if region in output:
+        f8s_regions.append(region)
+
+# Create a virtual machine in each eligible region using threads
+threads = []
+for region in f8s_regions:
+    t = threading.Thread(target=create_vm, args=(region,))
+    t.start()
+    threads.append(t)
+
+# Wait for all threads to complete
+for t in threads:
+    t.join()
+
+print("All virtual machines have been created and package list updated")
